2ec936bd3c637090a2444a98e01df62bb22fa0dd
Tropes-Ot <ot.server1@outlook.com>
2021-07-07 20:32:54 +0100

    starting project

---
+++ social_media_app/android/app/src/main/kotlin/com/btc/social_media_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.btc.social_media_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ social_media_app/lib/pages/feed.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Feed extends StatefulWidget {
+  @override
+  _FeedState createState() => _FeedState();
+}
+
+class _FeedState extends State<Feed> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
